e80fb9b6041fb1d1f3151cc1eeae159342e5a5e4
Callym <callym.cooke@gmail.com>
2016-04-07 21:40:37 +0100

    Add SetPosition, ChangePosition
    Also removes update code as it's not needed any more

--- PotentialHappiness/Camera.cs
+++ PotentialHappiness/Camera.cs
@@ -19,41 +19,36 @@ namespace PotentialHappiness
 
 		public void Update(GameTime gameTime)
 		{
-			int newX = X;
-			int newY = Y;
+			
+		}
 
-			int speed = 1;
+		public void SetPosition(int newX, int newY)
+		{
+			/*
+			the size of the border on each side of the camera
+			top-left sides have 1/nth border,
+			bottom-right have (n-1)/nth "border" - which is the amount of map visible
+			*/
+			int borderSize = 8;
+			int borderTL = PotentialHappinessGame.Instance.VirtualScreenSize / (borderSize * Scale);
+			int borderBR = borderTL * (borderSize - 1);
 
-			KeyboardState ks = Keyboard.GetState();
-			if (ks.IsKeyDown(Keys.Left))
-			{
-				newX += speed;
-			}
-			if (ks.IsKeyDown(Keys.Right))
-			{
-				newX -= speed;
-			}
-			if (ks.IsKeyDown(Keys.Up))
-			{
-				newY += speed;
-			}
-			if (ks.IsKeyDown(Keys.Down))
-			{
-				newY -= speed;
-			}
+			newX -= PotentialHappinessGame.Instance.VirtualScreenSize / (2 * Scale);
+			newY -= PotentialHappinessGame.Instance.VirtualScreenSize / (2 * Scale);
 
-			int border = PotentialHappinessGame.Instance.VirtualScreenSize / (2 * Scale);
-			int mapBorderX = MapManager.Instance.CurrentMap.MapWidth - border;
-			int mapBorderY = MapManager.Instance.CurrentMap.MapHeight - border;
+			int mapBorderX = MapManager.Instance.CurrentMap.MapWidth - borderBR;
+			int mapBorderY = MapManager.Instance.CurrentMap.MapHeight - borderBR;
 
-			if (newX < border && newX > -mapBorderX)
-			{
-				X = newX;
-			}
-			if (newY < border && newY > -mapBorderY)
-			{
-				Y = newY;
-			}
+			newX = -newX;
+			newY = -newY;
+
+			X = MathHelper.Clamp(newX, -mapBorderX, borderTL);
+			Y = MathHelper.Clamp(newY, -mapBorderY, borderTL);
+		}
+
+		public void ChangePosition(int newX = 0, int newY = 0)
+		{
+			SetPosition(X + newX, Y + newY);
 		}
 
 		public Point ToCamera(int x, int y)
