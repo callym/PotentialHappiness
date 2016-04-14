638be41cbeb78e08ed6239c122dabacb7b503434
Callym <callym.cooke@gmail.com>
2016-04-14 18:00:47 +0100

    Added LINQ extension for ForEachIndex
    Doesn't use a ForEach loop inside so you can modify elements of the list
    inside the loop (as that's why you'd want the index in a lot of cases?)

---
+++ PotentialHappiness/Extensions/LINQExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotentialHappiness.Extensions
+{
+	public static class LINQExtensions
+	{
+		public static void ForEachIndex<T>(this IEnumerable<T> ie, Action<T, int> action)
+		{
+			for (int i = 0; i < ie.Count(); i++)
+			{
+				action(ie.ElementAt(i), i);
+			}
+		}
+	}
+}
