b760b9668fd592b5b14ffe438710c3a3e51944ae
Callym <callym.cooke@gmail.com>
2016-04-14 16:38:11 +0100

    Wrap Random in a Manager

---
+++ PotentialHappiness/RandomManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotentialHappiness
+{
+	public sealed class RandomManager
+	{
+		public Random Random;
+
+		private RandomManager()
+		{
+			Random = new Random();
+		}
+
+		private static readonly Lazy<RandomManager> lazy = new Lazy<RandomManager>(() => new RandomManager());
+
+		public static RandomManager Instance => lazy.Value;
+	}
+}
